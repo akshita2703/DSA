760f7ee1387134567a8373eb939d50afb751d771
akshita2703 <rastogi32akshita.com>
2023-09-08 21:50:00 +0530

    added the concept of pass by value and pass by reference

---
+++ array/conceptOFpassbyValue.cpp
@@ -0,0 +1,20 @@
+
+// Concept of pass by value
+
+// copy banti h pass by value
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int m){
+    m=m*100;
+    cout<<m<<" function"<<endl;
+}
+
+
+int main(){
+    int n = 10;
+      cout<<n<<endl;
+    solve(n);
+      cout<<n<<" main"<<endl;
+}
